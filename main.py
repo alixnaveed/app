a448447a783100a2e52a9d71f9110295ef715474
alixnaveed <alin9866@gmail.com>
2023-06-09 14:15:51 +0500

    com

--- main.py
+++ main.py
@@ -44,4 +44,4 @@ async def read_root(request: Request):
     return response
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",host='0.0.0.0', port=9876, reload=True, debug=True)
+    uvicorn.run("main:app",host='0.0.0.0', port=9876, reload=True)
